0af6cfe8a04082533cf7aaba4b8c51c7f637a8f8
Samuel <145049239+SamuelSoCo@users.noreply.github.com>
2023-11-18 14:23:27 -0600

    estructura de iteracion para segunda interaccion con el menu

--- Sin Nombre1.cpp
+++ Sin Nombre1.cpp
@@ -1,61 +1,95 @@
-#include<iostream>
-#include<stdlib.h>
+#include <iostream>
+
 using namespace std;
-int a,b;
-int main()
-{
-	do{
-		cout<<"---------------------------------------------------------------"<<endl;
-	cout<<"                                                       HP:100%"<<endl;
-	cout<<"-juan es ungranjero algo alto y furte pero con mala suerte"<<endl;
-	cout<<"-parece que va adespertar.Ahhhhh(juan desperto),ve asu alrededor"<<endl;
-	cout<<"-juan:que tarde me desperte que hare hoy. "<<endl;
-	cout<<"---------------------------------------------------------------"<<endl;
-	cout<<"que quieres que haja juan pulsa los numeros que tiene cada orden.\n";
-	cout<<"1-Dormir un poco mas."<<endl;
-	cout<<"2-Levantarte cambiarse de ropa y salir."<<endl;
-	cout<<"3-Levantarse de la cama eir a desayunar."<<endl;
-	cout<<"4-Ver que hay en tu chosa y que puedes hacer dentro de ella;"<<endl;
-	cout<<"5-Salir del juego."<<endl;
 
-	 cin>>a;
-	 switch(a){
-	 	case 1:"Ya apasado varias horas ya es como la una dela tarde,ya no desayune bueno que hare entonces .\n";
-	 	"1-Te quieres levantar a buscar para  comer\n";
-	 	"2-Cambierte de ropa y salir de tu caso a ver tu campo como quedo de la lluvia de ayer\n";
-	 	"3-Ver que puedes hacer en tu casa para pasar el rato\n";
-	 	"4-Quieres salir."
-	 	break;
-	 	
-	 	case 2 :"hooo no la ropa limpia se quedo colgada afuera y como llovio no puedo cambiarme.\n";
-	 	break;
-	 	case 3 : "                                                                      hp:100%";
-		 "vere que tengo para desayunar parece que solo tengo pan tendre de salir a ordeñar una vaca si quiero leche para mi pan.\n";
-		 case 4 :"tengo en mi casa un libro(la biblia), un encendedor un cuchillo y una pistola\n";
-		 break;
-		case 5 :"seguro que quieres salir\n";
-		 cout<<"\n 1-si.";
-		 cout<<"\n 2-no.";
-		 cin>> b;
-		 if(b==1){
-		 	return 0;
-		 }
-		 else
-		 {return main();
-		 }
-		 break;
-	 }
-	}
-	while(b!=5);
-	system("pause");
+int main() {
+    char opcion;
 
-	}
-	
-	
+    do {
+        // Menú principal
+        cout << "\nMenú Principal:" << endl;
+        cout << "a. Acción 1" << endl;
+        cout << "b. Acción 2" << endl;
+        cout << "c. Acción 3" << endl;
+        cout << "d. Acción 4" << endl;
+        cout << "e. Salir" << endl;
+        cout << "Seleccione una opción (a-e): ";
+        cin >> opcion;
 
+        switch (opcion) {
+            case 'a':
+                // Acción 1
+                cout << "\nRealizando Acción 1:" << endl;
+                for (int i = 1; i <= 3; ++i) {
+                    cout << i << ". Submenú " << i << ".1" << endl;
+                    cout << i << ". Submenú " << i << ".2" << endl;
+                    cout << i << ". Submenú " << i << ".3" << endl;
+                    cout << i << ". Submenú " << i << ".4" << endl;
+                    cout << i << ". Submenú " << i << ".5" << endl;
+                    cout << "Seleccione una opción (1-5): ";
+                    cin >> opcion;
+                    // Agrega acciones para cada submenú
+                }
+                break;
 
-return 0;}
+            case 'b':
+                // Acción 2
+                cout << "\nRealizando Acción 2:" << endl;
+                for (int i = 1; i <= 3; ++i) {
+                    cout << i << ". Submenú " << i << ".1" << endl;
+                    cout << i << ". Submenú " << i << ".2" << endl;
+                    cout << i << ". Submenú " << i << ".3" << endl;
+                    cout << i << ". Submenú " << i << ".4" << endl;
+                    cout << i << ". Submenú " << i << ".5" << endl;
+                    cout << "Seleccione una opción (1-5): ";
+                    cin >> opcion;
+                    // Agrega acciones para cada submenú
+                }
+                break;
+
+            case 'c':
+                // Acción 3
+                cout << "\nRealizando Acción 3:" << endl;
+                for (int i = 1; i <= 3; ++i) {
+                    cout << i << ". Submenú " << i << ".1" << endl;
+                    cout << i << ". Submenú " << i << ".2" << endl;
+                    cout << i << ". Submenú " << i << ".3" << endl;
+                    cout << i << ". Submenú " << i << ".4" << endl;
+                    cout << i << ". Submenú " << i << ".5" << endl;
+                    cout << "Seleccione una opción (1-5): ";
+                    cin >> opcion;
+                    // Agrega acciones para cada submenú
+                }
+                break;
+
+            case 'd':
+                // Acción 4
+                cout << "\nRealizando Acción 4:" << endl;
+                for (int i = 1; i <= 3; ++i) {
+                    cout << i << ". Submenú " << i << ".1" << endl;
+                    cout << i << ". Submenú " << i << ".2" << endl;
+                    cout << i << ". Submenú " << i << ".3" << endl;
+                    cout << i << ". Submenú " << i << ".4" << endl;
+                    cout << i << ". Submenú " << i << ".5" << endl;
+                    cout << "Seleccione una opción (1-5): ";
+                    cin >> opcion;
+                    // Agrega acciones para cada submenú
+                }
+                break;
+
+            case 'e':
+                cout << "Saliendo del programa. ¡Hasta luego!" << endl;
+                break;
+
+            default:
+                cout << "Opción no válida. Inténtelo de nuevo." << endl;
+        }
+
+    } while (opcion != 'e');
+
+    return 0;
 }
+
 
 
 
