ba8d4b04126f58362f3253224e2fc964cdbdbf28
Samuel <145049239+SamuelSoCo@users.noreply.github.com>
2023-11-18 15:11:09 -0600

    nuevos comentarios y case

--- Sin Nombre1.cpp
+++ Sin Nombre1.cpp
@@ -7,26 +7,33 @@ int main() {
 
     do {
         // Menú principal
-        cout << "\nMenú Principal:" << endl;
-        cout << "a. Acción 1" << endl;
-        cout << "b. Acción 2" << endl;
-        cout << "c. Acción 3" << endl;
-        cout << "d. Acción 4" << endl;
-        cout << "e. Salir" << endl;
-        cout << "Seleccione una opción (a-e): ";
+        cout << "--------------------------------------------------------------    HP=100%" << endl;
+        cout<<"-juan es ungranjero algo alto y furte pero con mala suerte"<<endl;
+        cout<<"parece que va adespertar.Ahhhhh(juan desperto),ve asu alrededor"<<endl;
+        cout<<"-juan:que tarde me desperte que hare hoy. "<<endl;
+        cout<<"---------------------------------------------------------------"<<endl;
+        cout<<"que quieres que haga juan pulsa los numeros que tiene cada orden.\n";
+    
+
+        cout << "1.Dormir un poco mas. " << endl;
+        cout << "2. Levantarte cambiarse de ropa y salir. "<< endl;
+        cout << "3. Levantarse de la cama eir a desayunar." << endl;
+        cout << "4. Ver que hay en tu chosa y que puedes hacer dentro de ella." << endl;
+        cout << "5. Salir del juego." << endl;
+        cout << "Seleccione una opción (1-5): ";
         cin >> opcion;
 
         switch (opcion) {
-            case 'a':
+            case 1:cout << "--------------------------------------------------------------    HP=100%" << endl;
                 // Acción 1
-                cout << "\nRealizando Acción 1:" << endl;
+                cout << "Ya apasado varias horas ya es como la una dela tarde,ya no desayune bueno que hare entonces :" << endl;
                 for (int i = 1; i <= 3; ++i) {
-                    cout << i << ". Submenú " << i << ".1" << endl;
-                    cout << i << ". Submenú " << i << ".2" << endl;
-                    cout << i << ". Submenú " << i << ".3" << endl;
-                    cout << i << ". Submenú " << i << ".4" << endl;
-                    cout << i << ". Submenú " << i << ".5" << endl;
-                    cout << "Seleccione una opción (1-5): ";
+                    cout << i << "1-Te quieres levantar a buscar para  comer " << i << ".1" << endl;
+                    cout << i << "Cambierte de ropa y salir de tu caso a ver tu campo como quedo de la lluvia de ayer\n" << i << ".2" << endl;
+                    cout << i << "-Ver que puedes hacer en tu casa para pasar el rato" << i << ".3" << endl;
+                    
+                    
+                    cout << "Seleccione una opción (1-3): ";
                     cin >> opcion;
                     // Agrega acciones para cada submenú
                 }
@@ -34,13 +41,14 @@ int main() {
 
             case 'b':
                 // Acción 2
-                cout << "\nRealizando Acción 2:" << endl;
+                cout << "hooo no la ropa limpia se quedo colgada afuera y como llovio no puedo cambiarme .\n:" << endl;
+                cout<<"tendre que salir o desayunar mejor antes de salir\n";
                 for (int i = 1; i <= 3; ++i) {
-                    cout << i << ". Submenú " << i << ".1" << endl;
-                    cout << i << ". Submenú " << i << ".2" << endl;
-                    cout << i << ". Submenú " << i << ".3" << endl;
-                    cout << i << ". Submenú " << i << ".4" << endl;
-                    cout << i << ". Submenú " << i << ".5" << endl;
+                    cout << i << "1- Desayunar pan solo" << i << ".1" << endl;
+                    cout << i << "2-revisar tus cosas entes de salir" << i << ".2" << endl;
+                    cout << i << "3-salir asi sin desayunar(con la misma ropa que se puede hacer) " << i << ".3" << endl;
+                    
+                    
                     cout << "Seleccione una opción (1-5): ";
                     cin >> opcion;
                     // Agrega acciones para cada submenú
