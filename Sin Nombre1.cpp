537980ba74bda969f273afc654b099ceeb5e1fd2
Samuel <145049239+SamuelSoCo@users.noreply.github.com>
2023-11-18 00:30:28 -0600

    se agrego segunda linea de dialogos

--- Sin Nombre1.cpp
+++ Sin Nombre1.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
-#include<cstdlib>
+#include<stdlib.h>
 using namespace std;
-int elejir,eleccion;
+int a,b;
 int main()
 {
 	do{
-		cout<<"---------------------------------------------------------------";
+		cout<<"---------------------------------------------------------------"<<endl;
 	cout<<"                                                       HP:100%"<<endl;
 	cout<<"-juan es ungranjero algo alto y furte pero con mala suerte"<<endl;
 	cout<<"-parece que va adespertar.Ahhhhh(juan desperto),ve asu alrededor"<<endl;
@@ -15,44 +15,46 @@ int main()
 	cout<<"1-Dormir un poco mas."<<endl;
 	cout<<"2-Levantarte cambiarse de ropa y salir."<<endl;
 	cout<<"3-Levantarse de la cama eir a desayunar."<<endl;
-	cout<<"4-Ver que hay en tu cabaï¿½a y que puedes hacer dentro de ella;"<<endl;
+	cout<<"4-Ver que hay en tu chosa y que puedes hacer dentro de ella;"<<endl;
 	cout<<"5-Salir del juego."<<endl;
 
-	 cin>>elejir;
-		switch(elejir)
-		case 1:cout<<"1-dormir un poco mas."<<endl; 
-		break; 
-		case 2:
-		break;
-		case 3:cout<<""
-		break;
-		case 4:
-		break;
-		case 5:
-		cuot<<"seguro que quieres salir"
-		cout<<<"\n 1-SI";
-		cout<<"\n  2-NO";
-		cint>> elecion;
-		if(eleccion==1)
-		{return 0;
-		}
-		else{
-			return main()
-		}
-		exit(EXIT_SUCCESS);
-		break;
-		default :
-		cout<<"No ingreso un numero permitido"<<endl;
-		break;
+	 cin>>a;
+	 switch(a){
+	 	case 1:"Ya apasado varias horas ya es como la una dela tarde,ya no desayune bueno que hare entonces .\n";
+	 	"1-Te quieres levantar a buscar para  comer\n";
+	 	"2-Cambierte de ropa y salir de tu caso a ver tu campo como quedo de la lluvia de ayer\n";
+	 	"3-Ver que puedes hacer en tu casa para pasar el rato\n";
+	 	"4-Quieres salir."
+	 	break;
+	 	
+	 	case 2 :"hooo no la ropa limpia se quedo colgada afuera y como llovio no puedo cambiarme.\n";
+	 	break;
+	 	case 3 : "                                                                      hp:100%";
+		 "vere que tengo para desayunar parece que solo tengo pan tendre de salir a ordeñar una vaca si quiero leche para mi pan.\n";
+		 case 4 :"tengo en mi casa un libro(la biblia), un encendedor un cuchillo y una pistola\n";
+		 break;
+		case 5 :"seguro que quieres salir\n";
+		 cout<<"\n 1-si.";
+		 cout<<"\n 2-no.";
+		 cin>> b;
+		 if(b==1){
+		 	return 0;
+		 }
+		 else
+		 {return main();
+		 }
+		 break;
+	 }
 	}
-	while(elejir!=5);
+	while(b!=5);
 	system("pause");
 
 	}
 	
 	
 
-	return 0;
+
+return 0;}
 }
 
 
