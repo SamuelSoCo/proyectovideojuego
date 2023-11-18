0bf59b19d2187a62bb5386f256a1da0065d4716d
Samuel <145049239+SamuelSoCo@users.noreply.github.com>
2023-11-18 15:56:52 -0600

    inicio de la tercera iteracion del personaje
    i

--- Sin Nombre1.cpp
+++ Sin Nombre1.cpp
@@ -29,12 +29,57 @@ int main() {
                 cout << "Ya apasado varias horas ya es como la una dela tarde,ya no desayune bueno que hare entonces :" << endl;
                 for (int i = 1; i <= 3; ++i) {
                     cout << i << "1-Te quieres levantar a buscar para  comer " << i << ".1" << endl;
-                    cout << i << "Cambierte de ropa y salir de tu caso a ver tu campo como quedo de la lluvia de ayer\n" << i << ".2" << endl;
-                    cout << i << "-Ver que puedes hacer en tu casa para pasar el rato" << i << ".3" << endl;
+                    cout << i << "2-Cambierte de ropa y salir de tu caso a ver tu campo como quedo de la lluvia de ayer\n" << i << ".2" << endl;
+                    cout << i << "3-Ver que puedes hacer en tu casa para pasar el rato" << i << ".3" << endl;
                     
                     
                     cout << "Seleccione una opción (1-3): ";
                     cin >> opcion;
+                     switch (opcion) {
+            case 1:
+                // Submenú 1
+                int opcion;
+                do {
+                    cout << "---------------------------------------------------------Dinero:¿?   Arma:ninguna    HP=100%" << endl;
+					cout << "parece que solo tengo pan con agua tendre que comer esto o salgo a ordeñar una vaca?" << endl;
+                    cot<<"me conformo con esto o salgo a buscar otra cosa de comer o veo mi avitacion"<<endl;
+                    // Agrega las opciones del submenú 1 aquí
+                    cout << "1-comer el pan cambiarse de ropa y salir a ver tu campo despues de la lluvia de ayer\n" << endl;
+                    cout << "2.salir a buscar otra cosa que comer." << endl;
+                    cout << "3. salir a ordeñar la vaca para regresar a desayunar tu pan?" << endl;
+                    cout << "Seleccione una opción: ";
+                    cin >> opcion;
+
+                    switch (opcionSubmenu1) {
+                        // Agrega casos para las opciones del submenú 1
+                        case 1:
+                            cout << "Realizando tarea para la Opción 1 del Submenú 1." << endl;
+                            break;
+                        case 2:
+                            cout << "Realizando tarea para la Opción 2 del Submenú 1." << endl;
+                            break;
+                        // Agrega más casos según sea necesario
+
+                    }
+                } while (opcionSubmenu1 != 3);
+                break;
+
+            case 2:
+                // Submenú 2
+                // Repite el patrón para otros submenús
+                break;
+
+            case 3:
+                // Submenú 3
+                // Repite el patrón para otros submenús
+                break;
+
+            case 4:
+                cout << "Saliendo del programa. ¡Hasta luego!" << endl;
+                break;
+
+            default:
+                cout << "Opción no válida. Inténtelo de nuevo." << endl;
                     // Agrega acciones para cada submenú
                 }
                 break;
@@ -80,9 +125,9 @@ int main() {
                     cout << i << "1-Salir a comprar municion para el revolver." << i << ".1" << endl;
                     cout << i << "2-Ver que hay para desayunar y salir al campo. " << i << ".2" << endl;
                     cout << i << "3-Leer el libro un rato." << i << ".3" << endl;
-                    cout << i << ". Submenú " << i << ".4" << endl;
-                    cout << i << ". Submenú " << i << ".5" << endl;
-                    cout << "Seleccione una opción (1-5): ";
+                    
+                    
+                    cout << "Seleccione una opción (1-3): ";
                     cin >> opcion;
                     // Agrega acciones para cada submenú
                 }
