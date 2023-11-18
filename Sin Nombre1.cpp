70a4099d5c1d0fed3618f901f0d7d1c6a2e16ede
Samuel <145049239+SamuelSoCo@users.noreply.github.com>
2023-11-17 23:26:07 -0600

    Add files via upload
    inicio de programar el juego e ingreso de dialogos

--- Sin Nombre1.cpp
+++ Sin Nombre1.cpp
@@ -1,9 +1,70 @@
-#include<iostream.h>
+#include<iostream>
+#include<cstdlib>
 using namespace std;
-intmain(){
+int elejir,eleccion;
+int main()
+{
 	do{
-		swicth
+		cout<<"---------------------------------------------------------------";
+	cout<<"                                                       HP:100%"<<endl;
+	cout<<"-juan es ungranjero algo alto y furte pero con mala suerte"<<endl;
+	cout<<"-parece que va adespertar.Ahhhhh(juan desperto),ve asu alrededor"<<endl;
+	cout<<"-juan:que tarde me desperte que hare hoy. "<<endl;
+	cout<<"---------------------------------------------------------------"<<endl;
+	cout<<"que quieres que haja juan pulsa los numeros que tiene cada orden.\n";
+	cout<<"1-Dormir un poco mas."<<endl;
+	cout<<"2-Levantarte cambiarse de ropa y salir."<<endl;
+	cout<<"3-Levantarse de la cama eir a desayunar."<<endl;
+	cout<<"4-Ver que hay en tu caba�a y que puedes hacer dentro de ella;"<<endl;
+	cout<<"5-Salir del juego."<<endl;
+
+	 cin>>elejir;
+		switch(elejir)
+		case 1:cout<<"1-dormir un poco mas."<<endl; 
+		break; 
+		case 2:
+		break;
+		case 3:cout<<""
+		break;
+		case 4:
+		break;
+		case 5:
+		cuot<<"seguro que quieres salir"
+		cout<<<"\n 1-SI";
+		cout<<"\n  2-NO";
+		cint>> elecion;
+		if(eleccion==1)
+		{return 0;
+		}
+		else{
+			return main()
+		}
+		exit(EXIT_SUCCESS);
+		break;
+		default :
+		cout<<"No ingreso un numero permitido"<<endl;
+		break;
 	}
-	while
+	while(elejir!=5);
+	system("pause");
+
+	}
+	
+	
+
 	return 0;
 }
+
+
+
+	
+	
+
+
+	
+
+
+	
+
+
+
